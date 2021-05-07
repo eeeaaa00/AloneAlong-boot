24535d18795276ee0fa29dad483095646836e1b4
JisuKong <kongjisu52@gmail.com>
2021-05-08 02:22:08 +0900

    Update Domain, Mapper & View

--- src/main/java/com/dwu/alonealong/domain/Product.java
+++ src/main/java/com/dwu/alonealong/domain/Product.java
@@ -9,65 +9,63 @@ public class Product implements Serializable {
 
   private String productId;
   private String pcId; //productCategoryId
-  private String name;
-  private String info;
-  private String img;
-  private String date;
-  private String contents;
-  private int price;
-  private int sales;
-  private int stock;
+  private String productName;
+  private String productInfo;
+  private String productDate;
+  private byte[] productImg;
+  private byte[] productContents;
+  private int productPrice;
+  private int productSales;
+  private int productStock;
   private int shippingFee;
   private int quantity;
   
   /* JavaBeans Properties */
-
   public String getProductId() { return productId; }
-  public void setProductId(String productId) { this.productId = productId.trim(); }
-
+  public void setProductId(String productId) { this.productId = productId; }
+  
   public String getPcId() { return pcId; }
   public void setPcId(String pcId) { this.pcId = pcId; }
   
-  public String getName() { return name; }
-  public void setName(String name) { this.name = name; }
+  public String getProductName() { return productName; }
+  public void setProductName(String productName) { this.productName = productName; }
   
-  public String getInfo() { return info; }
-  public void setInfo(String info) { this.info = info; }
+  public String getProductInfo() { return productInfo; }
+  public void setProductInfo(String productInfo) { this.productInfo = productInfo; }
   
-  public String getImg() { return img; }
-  public void setImg(String img) { this.img = img; }
+  public String getProductDate() { return productDate; }
+  public void setProductDate(String productDate) { this.productDate = productDate; }
   
-  public String getDate() { return date; }
-  public void setDate(String date) { this.date = date; }
+  public byte[] getProductImg() { return productImg; }
+  public void setProductImg(byte[] productImg) { this.productImg = productImg; }
   
-  public String getContents() { return contents; }
-  public void setContents(String contents) { this.contents = contents; }
+  public byte[] getProductContents() { return productContents; }
+  public void setProductContents(byte[] productContents) { this.productContents = productContents; }
   
-  public int getPrice() { return price; }
-  public void setPrice(int price) { this.price = price; }
+  public int getProductPrice() { return productPrice; }
+  public void setProductPrice(int productPrice) { this.productPrice = productPrice; }
   
-  public int getSales() { return sales; }
-  public void setSales(int sales) { this.sales = sales; }
+  public int getProductSales() { return productSales; }
+  public void setProductSales(int productSales) { this.productSales = productSales; }
   
-  public int getStock() { return stock; }
-  public void setStock(int stock) { this.stock = stock; }
+  public int getProductStock() { return productStock; }
+  public void setProductStock(int productStock) { this.productStock = productStock; }
   
   public int getShippingFee() { return shippingFee; }
   public void setShippingFee(int shippingFee) { this.shippingFee = shippingFee; }
-
   
   public int getQuantity() { return quantity; }
-  public void setQuantity(int quantity) {	this.quantity = quantity; }
+  public void setQuantity(int quantity) { this.quantity = quantity; }
 
   /*Additional Methods*/
   public boolean isInStock() {
-	  if (stock == 0) return true;
+	  if (productStock == 0) return true;
 	  return false;
   }
 
-  public int getUnitPrice() {
+public int getUnitPrice() {
 	  int unitPrice = 0;
-	  unitPrice += quantity * price;
+	  unitPrice += quantity * productPrice;
 	  if (unitPrice <= 30000) {
 		  unitPrice += shippingFee;
 	  }
@@ -76,6 +74,6 @@ public class Product implements Serializable {
   
   /* Public Methods*/
 	public String toString() {
-    return getName();
+    return getProductName();
   }
 }
