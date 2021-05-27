0291ab8acc129c628aa7695ff8995ac983edf79a
the100-00 <alak10@naver.com>
2021-05-27 18:23:04 +0900

    feat:Tiles 적용

--- src/main/java/com/dwu/alonealong/controller/ViewMypageController.java
+++ src/main/java/com/dwu/alonealong/controller/ViewMypageController.java
@@ -1,6 +1,7 @@
 package com.dwu.alonealong.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,40 +9,39 @@ import org.springframework.web.servlet.ModelAndView;
 public class ViewMypageController {
 
 	@RequestMapping(value = "/mypage")
-	public ModelAndView mypage() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/mypage/myInfo");
-		return mav;
+	public String handleRequest(ModelMap model) throws Exception {
+
+		return "mypage";
 	}
-	
+
 	@RequestMapping(value = "/myPayment")
 	public ModelAndView myPayment() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/mypage/myPayment");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/myOrder")
 	public ModelAndView myOrder() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/mypage/myOrder");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/myFood")
 	public ModelAndView myFood() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/mypage/myFood");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/myTogether")
 	public ModelAndView myTogether() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/mypage/myTogether");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/myRestaurant")
 	public ModelAndView myRestaurant() {
 		ModelAndView mav = new ModelAndView();
